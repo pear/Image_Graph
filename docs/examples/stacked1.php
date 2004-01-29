7d95a5e8d6cc9704c3fc01436f0d76dc28cd0f9e
Stefan Neufeind <neufeind@php.net>
2004-01-29 02:26:30 +0000

    preparation for stacking of data (not yet fully finished)
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_Graph/trunk@150084 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ docs/examples/stacked1.php
@@ -0,0 +1,82 @@
+<?
+// $Id$
+/**
+* Example for using the Image_Graph-class
+*
+* @author   Stefan Neufeind <pear.neufeind@speedpartner.de>
+* @package  Image_Graph
+*/
+
+    require_once("Image/Graph.php");
+
+    $graph = new Image_Graph (400, 200);
+    $graph->setBackgroundColor (array(0x59, 0x59, 0x59));
+    $graph->setSpaceFromBorder (5); // 5 pixels from each border
+    $graph->setDefaultFontOptions (array("fontPath" => "", "fontFile" => "arial", "antiAliasing" => true, "fontSize" => 10, "color" => array(0,0,0) ));
+    $graph->diagramTitle->setColor (array(0xBF,0xBF,0xBF));
+    $graph->diagramTitle->setSpacer (array("bottom" => 10));
+    $graph->axeX->title->setSpacer (array("top" => 10, "bottom" => 0));
+
+    $graph->diagramTitle->setText ("Downloads June 2003");
+    $graph->axeX->title->setText ("Days");
+    $graph->axeY0->setNumberformat ("%.1f MB");
+    $graph->axeY1->setNumberformat ("%.0f files");
+
+    $graph->axeY0->title->setText ("Traffic");
+    $graph->axeY1->title->setText ("Number of downloads");
+
+    $graph->axeY0->setNumbercolor (array(0xCC,0x29,0x29));
+    $graph->axeY1->setNumbercolor (array(0x29,0xCC,0x29));
+
+    $graph->axeY0->title->setColor (array(0xCC,0x29,0x29));
+    $graph->axeY1->title->setColor (array(0x29,0xCC,0x29));
+
+    $graph->axeY0->title->setFontOptions (array("fontSize" => 8));
+    $graph->axeY1->title->setFontOptions (array("fontSize" => 8));
+
+    $graph->setAxesColor(array(0x40, 0x40, 0xFF));
+
+    $data    = array( 20,
+                      42,
+                      58
+                    );
+
+    $graph->setDataDefaultColor(array(0xCC,0x29,0x29));
+//    $graph->addData($data, "line", array("axeId" => 0));
+    $graph->addData($data, "bar",  array("axeId" => 1, "color" => array(0xBF,0xBF,0x30)));
+
+    $data    = array( 15.8,
+                      37.2,
+                      50.0
+                    );
+
+    $graph->addData($data, "bar",  array("axeId" => 1, "color" => array(0x30,0xBF,0xBF)));
+    $dataObj = &$graph->addData($data, "line", array("axeId" => 1, "color" => array(0x29,0xCC,0x29)));
+    $marker =& $dataObj->setDataMarker("diamond", array("color" => array(0xBF,0xBF,0xBF)));
+    $marker->setSize(7);
+
+    $data    = array( 10,
+                      12,
+                      17
+                    );
+
+    $graph->addData($data, "line",  array("axeId" => 0, "color" => array(0x30,0xBF,0xBF)));
+    
+    $graph->axeX->setLabels(array("Month 1", "Month 2", "Month 3"));
+
+    $graph->axeY1->setBounds(0,100);
+    $graph->axeY1->setTicksAutoSteps(10);
+
+    $graph->axeY1->setTickStyle(IMAGE_GRAPH_TICKS_BOTH);
+    $graph->axeY1->setTickSize (5);
+
+    $graph->axeX->setTickStyle(IMAGE_GRAPH_TICKS_BOTH);
+    $graph->axeX->setTickSize (5);
+
+    $graph->stackData("bar");
+
+    $image = $graph->getGDImage();
+
+    Header ("Content-Type: image/png");
+    imagepng($image);
+?>
