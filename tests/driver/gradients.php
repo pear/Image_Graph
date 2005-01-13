fd85fdb912f03a0e3513ab9ac4440ce3ee886acd
Jesper Veggerby Hansen <nosey@php.net>
2005-01-13 12:49:33 +0000

    Added visual test cases
    git-svn-id: http://svn.php.net/repository/pear/packages/Image_Graph/trunk@177117 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/driver/gradients.php
@@ -0,0 +1,96 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | Image_Graph                                                              |
+// +--------------------------------------------------------------------------+
+// | Copyright (C) 2003, 2004 Jesper Veggerby                                 |
+// | Email         pear.nosey@veggerby.dk                                     |
+// | Web           http://pear.veggerby.dk                                    |
+// | PEAR          http://pear.php.net/package/Image_Graph                    |
+// +--------------------------------------------------------------------------+
+// | This library is free software; you can redistribute it and/or            |
+// | modify it under the terms of the GNU Lesser General Public               |
+// | License as published by the Free Software Foundation; either             |
+// | version 2.1 of the License, or (at your option) any later version.       |
+// |                                                                          |
+// | This library is distributed in the hope that it will be useful,          |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        |
+// | Lesser General Public License for more details.                          |
+// |                                                                          |
+// | You should have received a copy of the GNU Lesser General Public         |
+// | License along with this library; if not, write to the Free Software      |
+// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA |
+// +--------------------------------------------------------------------------+
+
+/**
+ * This is a visual test case, testing driver support for gradient fillings.
+ * 
+ * Passing a GET parameter with the driver type switches drivers so that other
+ * driver types can be tested, i.e.:
+ * 
+ * http://.../gradients.php?driver=(png|jpg|gif|wbmp|svg|pdflib)
+ *
+ * @package Image_Graph
+ * @subpackage Tests
+ * @category images
+ * @copyright Copyright (C) 2003, 2004 Jesper Veggerby Hansen
+ * @license http://www.gnu.org/licenses/lgpl.txt GNU Lesser General Public License
+ * @author Jesper Veggerby <pear.nosey@veggerby.dk>
+ * @version $Id$
+ */
+
+include 'Image/Graph/Driver.php';
+
+$driver =& Image_Graph_Driver::factory(
+    (isset($_GET['driver']) ? $_GET['driver'] : 'png'), 
+    array('width' => 605, 'height' => 350)
+);
+
+$gradient = array(
+    'type' => 'gradient', 
+    'start' => 'yellow', 
+    'end' => 'maroon' 
+);
+
+$space = 10;
+$size = 75;
+
+$driver->setLineColor('black');
+$driver->rectangle(0, 0, $driver->getWidth() - 1, $driver->getHeight() - 1);
+
+for ($i = 0; $i < 7; $i ++) {// there are 7 different gradient types
+    $gradient['direction'] = $i + 1;
+
+    $x = $space + ($i * ($size + $space));
+    
+    $y = $space;
+    $driver->setGradientFill($gradient);    
+    $driver->rectangle($x, $y, $x + $size, $y + $size);
+
+    $y += $size + $space;
+    $driver->setGradientFill($gradient);    
+    $driver->ellipse($x + $size / 2, $y + $size / 2, $size / 2, $size / 2);      
+
+    $y += $size + $space;
+    $driver->setGradientFill($gradient);    
+    $driver->pieSlice($x + $size / 2, $y + $size / 2, $size / 2, $size / 2, 45, 270);
+   
+    $y += $size + $space;
+    $points = array();
+    $points[] = array('x' => $x + $size / 3, 'y' => $y);
+    $points[] = array('x' => $x + $size, 'y' => $y + $size / 2);
+    $points[] = array('x' => $x + $size / 3, 'y' => $y + 3 * $size / 4);
+    $points[] = array('x' => $x + $size / 5, 'y' => $y + $size);
+    $points[] = array('x' => $x, 'y' => $y + $size / 3);
+    $y += $size + $space;
+    $driver->setGradientFill($gradient);
+    foreach ($points as $point) {
+        $driver->polygonAdd($point['x'], $point['y']);
+    }
+    $driver->polygonEnd(true);    
+
+}
+        
+$driver->done();
+
+?>
---
+++ tests/driver/png.php
@@ -0,0 +1,46 @@
+<?php
+// +--------------------------------------------------------------------------+
+// | Image_Graph                                                              |
+// +--------------------------------------------------------------------------+
+// | Copyright (C) 2003, 2004 Jesper Veggerby                                 |
+// | Email         pear.nosey@veggerby.dk                                     |
+// | Web           http://pear.veggerby.dk                                    |
+// | PEAR          http://pear.php.net/package/Image_Graph                    |
+// +--------------------------------------------------------------------------+
+// | This library is free software; you can redistribute it and/or            |
+// | modify it under the terms of the GNU Lesser General Public               |
+// | License as published by the Free Software Foundation; either             |
+// | version 2.1 of the License, or (at your option) any later version.       |
+// |                                                                          |
+// | This library is distributed in the hope that it will be useful,          |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of           |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU        |
+// | Lesser General Public License for more details.                          |
+// |                                                                          |
+// | You should have received a copy of the GNU Lesser General Public         |
+// | License along with this library; if not, write to the Free Software      |
+// | Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA |
+// +--------------------------------------------------------------------------+
+
+/**
+ * This is a visual test case, testing driver fundamental driver functionality.
+ *
+ * @package Image_Graph
+ * @subpackage Tests
+ * @category images
+ * @copyright Copyright (C) 2003, 2004 Jesper Veggerby Hansen
+ * @license http://www.gnu.org/licenses/lgpl.txt GNU Lesser General Public License
+ * @author Jesper Veggerby <pear.nosey@veggerby.dk>
+ * @version $Id$
+ */
+
+// SPECIFY HERE WHERE A TRUETYPE FONT CAN BE FOUND
+$testFont = 'c:/windows/fonts/Arial.ttf';
+
+include 'Image/Graph/Driver.php';
+
+$driver =& Image_Graph_Driver::factory('png', array('width' => 600, 'height' => 600));
+
+include './driver_body.php';
+
+?>
